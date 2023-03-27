a338e64af039bbf5732268718c6ccfc5a18d5f86
MariamPhavlenishvili <mariam.pavlenishvili.1@btu.edu.ge>
2023-03-27 17:06:22 +0400

    search changes

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { useState } from 'react';
 import styles from "./search.module.css";
 
 import SearchIcon from "@mui/icons-material/Search";
 
 function SearchBar() {
+  const [keyword, setKeyword] = useState("")
+
+  const onChange = (value: string) => {
+    setKeyword(value)
+  };
+
   return (
     <div className={styles.searchbar}>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => onChange(e.target.value)}
+      />
       <button type="submit">
-        <SearchIcon className={styles.icon}/>
+        <SearchIcon className={styles.icon} />
       </button>
     </div>
   );
