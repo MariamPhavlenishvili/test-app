cafda959a8d307181b5319093baa0a7f7fae7749
dmarjapplmacb3 <dmarjapplmacb3@gmail.com>
2023-04-04 18:13:06 +0400

    .

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { dataHubService, SiteName } from "data-hub";
 
-dataHubService.init(SiteName.MyAuto);
+dataHubService.init(SiteName.MyAuto, true);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
