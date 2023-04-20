ad477222adfc66969b5975ce0ae048be387d43c4
dmarjapplmacb3 <dmarjapplmacb3@gmail.com>
2023-04-20 16:13:20 +0400

    .

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { dataHubService, PlatformName } from "tnet-clickstream-connector";
+import { dataHubService } from "tnet-clickstream-connector";
 
-dataHubService.activate(PlatformName.MyAuto, true);
+dataHubService.activate(true);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
